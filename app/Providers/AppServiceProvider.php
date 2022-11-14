075bdba5d27f5ffef2f736dbef565111461948b5
Khuram Naqvi <anaqvi527@gmail.com>
2022-11-14 19:03:53 +0500

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         $url=URL('/');
-        dd($url);
+      
       if($url == 'https://walrus-app-tcqpr.ondigitalocean.app/') {
         \Illuminate\Support\Facades\URL::forceScheme('https');
        
