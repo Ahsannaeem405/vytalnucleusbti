1c90aa41243283d30267d887efd2aad6d597bb45
Khuram Naqvi <anaqvi527@gmail.com>
2022-11-16 19:34:24 +0500

    ffggf

--- app/Http/Controllers/Import.php
+++ app/Http/Controllers/Import.php
@@ -95,6 +95,8 @@ class Import extends Controller
                 $product->sku=$importData[9];
                 $product->tag=$importData[13];
                 $product->save();
+                
+
  
               }
 
